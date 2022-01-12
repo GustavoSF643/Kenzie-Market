a9f023d90ea961e743eff8a39481968ca1b52d2d
Gustavo Silva <gustavosilvafranco643@gmail.com>
2022-01-12 16:12:17 -0300

    feat: CreateProductService created

---
+++ src/services/Product/CreateProductService.ts
@@ -0,0 +1,24 @@
+import { getRepository } from "typeorm";
+import Product from "../../entities/Product";
+
+interface Request {
+    name: string;
+    price: number;
+    description: string;
+};
+
+export default class CreateProductService {
+    public async execute({ description, name, price }: Request): Promise<Product> {
+        const productRepository = getRepository(Product);
+
+        const product = productRepository.create({
+            description,
+            name,
+            price,
+        });
+
+        await productRepository.save(product);
+
+        return product;
+    };
+};
