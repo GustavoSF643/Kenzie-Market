d65dced9fe7eb58a147610e849e883d6f42c79eb
Gustavo Silva <gustavosilvafranco643@gmail.com>
2022-01-12 16:00:31 -0300

    feat: userAuth middleware created

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { create, list, retrieve } from "../controllers/UserController";
 import { admAuth } from "../middlewares/authentication/AdmAuth";
+import userAuth from "../middlewares/authentication/userAuth";
 
 const userRouter = Router();
 
 userRouter.post('/', create);
-userRouter.get('/:id', retrieve); // user só pode acessar seu perfil. Adm pode acessar qualquer user
+userRouter.get('/:id', userAuth,retrieve);
 userRouter.get('/', admAuth, list);
 
 export default userRouter;
